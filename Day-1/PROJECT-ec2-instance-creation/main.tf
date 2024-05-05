aef3ecf585d2f68157446de4cf04f87aeb78e8be
harshaprasad21 <harshaprasad20.hp@gmail.com>
2024-05-05 10:25:27 +0530

    us-east-2

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,9 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "us-east-2"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
     ami           = "ami-0f30a9c3a48f3fa79"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
+
